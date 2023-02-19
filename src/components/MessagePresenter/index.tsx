b9c9d5d067a67a182ad6ca1739ec4956a2f3ef33
ppantisawat <pakanon.pantisawat@agoda.com>
2023-02-20 01:25:48 +0700

    feat: add layout animation

--- src/components/MessagePresenter/index.tsx
+++ src/components/MessagePresenter/index.tsx
@@ -1,12 +1,16 @@
 import {useMessageReceiver} from '../../hooks/useMessageReceiver';
+import ReactTextTransition from "react-text-transition";
+import './message-presenter.css';
 
 const MessagePresenter = () => {
     const {message} = useMessageReceiver();
 
     return (
-        <p className="w-4/5 text-white text-center text-lg md:text-5xl relative mx-auto top-1/2 inset-0">
+        <ReactTextTransition
+            className="w-4/5 text-white text-center text-lg md:text-5xl relative mx-auto top-1/2 inset-0"
+        >
             {message}
-        </p>
+        </ReactTextTransition>
     );
 };
 
