69a9ef592b50fe158c27042271ff74c897917bb9
ppantisawat <pakanon.pantisawat@agoda.com>
2023-02-23 14:40:07 +0700

    refactor: defer page load

--- src/components/MessagePresenter/index.tsx
+++ src/components/MessagePresenter/index.tsx
@@ -10,12 +10,18 @@ const MessagePresenter = () => {
             <ReactTextTransition
                 className="w-full text-white text-center text-lg md:text-3xl text-center absolute inset-0 mx-auto"
             >
-                <div>{message}</div>
-                <div className="flex justify-center mt-4">
-                    <div className="w-10 border-b border-b-1 border-b-black -translate-y-3 mr-4"></div>
-                    <div className="text-base">{writer}</div>
-                    <div className="w-10 border-b border-b-1 border-b-black -translate-y-3 ml-4"></div>
-                </div>
+                {
+                    (!!message && !!writer) ? (
+                        <>
+                            <div>{message}</div>
+                            <div className="flex justify-center mt-4">
+                                <div className="w-10 border-b border-b-1 border-b-black -translate-y-3 mr-4"></div>
+                                <div className="text-base">{writer}</div>
+                                <div className="w-10 border-b border-b-1 border-b-black -translate-y-3 ml-4"></div>
+                            </div>
+                        </>
+                    ) : null
+                }
             </ReactTextTransition>
         </div>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,12 @@ import {RouterProvider} from 'react-router-dom';
 import router from './router';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <RouterProvider router={router}/>
-    </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+window.onload = () => {
+    root.render(
+        <React.StrictMode>
+            <RouterProvider router={router}/>
+        </React.StrictMode>,
+    );
+};
