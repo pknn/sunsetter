87ee72b25d5311ab9d48521728902a1fec03c036
ppantisawat <pakanon.pantisawat@agoda.com>
2023-02-23 14:29:02 +0700

    feat: update ThankYou page

--- src/ThankYou.tsx
+++ src/ThankYou.tsx
@@ -1,8 +1,27 @@
-const ThankYou = () => (
-    <div
-        className="publisher-box w-screen h-screen px-8 py-12 relative flex justify-center items-center text-white text-3xl">
-        Thank you
-    </div>
-);
+import loveLetter from './assets/love-letter.png';
+import sendNewLetterButton from './assets/send-new-letter-button.png';
+import {useNavigate} from 'react-router-dom';
+
+const ThankYou = () => {
+    const navigate = useNavigate();
+
+    const handleSendNewLetterButtonClick = () => {
+        navigate('/send-love');
+    };
+
+    return (
+        <div
+            className="publisher-box w-screen h-screen px-8 py-12 relative flex flex-col gap-8 justify-center items-center text-white text-3xl">
+            <div>
+                <img src={loveLetter}/>
+            </div>
+            <div>
+                <button onClick={handleSendNewLetterButtonClick}>
+                    <img src={sendNewLetterButton}/>
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default ThankYou;
